6eed6ade8ae5a9de741c3d58bd889d7a390f42fc
Keyonca <148034804+Keyonca@users.noreply.github.com>
2024-10-24 18:46:46 +0500

    Add files via upload

---
+++ Lab2_2_3/LineSegment.cs
@@ -0,0 +1,74 @@
+﻿public class LineSegment
+{
+    private double x1; // Координата начала отрезка
+    private double x2; // Координата конца отрезка
+
+    // Конструктор класса
+    public LineSegment(double start, double end)
+    {
+        // Убедимся, что начало меньше конца
+        if (start < end)
+        {
+            x1 = start;
+            x2 = end;
+        }
+        else
+        {
+            x1 = end;
+            x2 = start;
+        }
+    }
+
+    // Метод для проверки, попадает ли число в отрезок
+    public bool Contains(double number)
+    {
+        return number >= x1 && number <= x2;
+    }
+
+    // Перегрузка метода ToString()
+    public override string ToString()
+    {
+        return $"Отрезок: [{x1}, {x2}]";
+    }
+
+    // Унарная операция: вычислить длину отрезка
+    public static double operator !(LineSegment segment)
+    {
+        return segment.x2 - segment.x1;
+    }
+
+    // Унарная операция: увеличить координаты границ отрезка на 1
+    public static LineSegment operator ++(LineSegment segment)
+    {
+        return new LineSegment(segment.x1 + 1, segment.x2 + 1);
+    }
+
+    // Явное преобразование в int
+    public static explicit operator int(LineSegment segment)
+    {
+        return (int)segment.x1; // Возвращаем целую часть координаты начала отрезка
+    }
+
+    // Неявное преобразование в double
+    public static implicit operator double(LineSegment segment)
+    {
+        return segment.x2; // Возвращаем координату конца отрезка
+    }
+
+    // Бинарная операция: + (увеличение координат на d)
+    public static LineSegment operator +(LineSegment segment, int d)
+{
+    return new LineSegment(segment.x1 + d, segment.x2 + d);
+}
+
+    // Бинарная операция: < (проверка, попадает ли целое число в отрезок)
+    public static bool operator <(LineSegment segment, int number)
+    {
+        return segment.Contains(number);
+    }
+
+    public static bool operator >(LineSegment segment, int number)
+    {
+        return !segment.Contains(number); //Возвращаем true, если число не в отрезке
+    }
+}
---
+++ Lab2_2_3/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double start = 0;
+        double end = 0;
+
+        // Ввод данных от пользователя для начала отрезка
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Введите координаты начала отрезка:");
+                start = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                break; // Выход из цикла, если ввод корректен
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: некорректный ввод. Пожалуйста, введите числовое значение.");
+                Console.WriteLine("");
+            }
+        }
+
+        // Ввод данных от пользователя для конца отрезка
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Введите координаты конца отрезка:");
+                end = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                break; // Выход из цикла, если ввод корректен
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: некорректный ввод. Пожалуйста, введите числовое значение.");
+                Console.WriteLine("");
+            }
+        }
+
+        // Создание экземпляра класса LineSegment
+        LineSegment segment = new LineSegment(start, end);
+        Console.WriteLine(segment.ToString()); // Выводим информацию об отрезке 
+        // Проверка длины отрезка
+        double length = !segment;
+        Console.WriteLine($"Длина отрезка: {length}");
+        Console.WriteLine("");
+
+        // Увеличение координат границ отрезка на 1
+        segment++;
+        Console.WriteLine($"Координаты после увеличения на 1: {segment}");
+        Console.WriteLine("");
+
+        // Преобразование в int
+        int intPart = (int)segment;
+        Console.WriteLine($"Целая часть координаты начала отрезка: {intPart}");
+        Console.WriteLine("");
+
+        // Преобразование в double
+        double doublePart = segment;
+        Console.WriteLine($"Координата конца отрезка как double: {doublePart}");
+        Console.WriteLine("");
+
+        // Ввод числа для проверки
+        double number = 0;
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Введите число для проверки:");
+                number = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                break; // Выход из цикла, если ввод корректен
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: некорректный ввод. Пожалуйста, введите числовое значение.");
+                Console.WriteLine("");
+            }
+        }
+
+        // Проверка, попадает ли число в отрезок
+        if (segment < (int)number)
+        {
+            Console.WriteLine($"Число {number} попадает в отрезок.");
+            Console.WriteLine("");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} не попадает в отрезок.");
+            Console.WriteLine("");
+        }
+
+        // Проверка бинарной операции с увеличением координат на целое число
+        // Ввод числа для увеличения координат отрезка
+        int incrementValue = 0;
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("Введите целое число для увеличения координат отрезка:");
+                incrementValue = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                break; // Выход из цикла, если ввод корректен
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: некорректный ввод. Пожалуйста, введите целое числовое значение.");
+                Console.WriteLine("");
+            }
+        }
+
+        // Проверка бинарной операции с увеличением координат на введенное число
+        LineSegment incrementedSegment = segment + incrementValue;
+        Console.WriteLine($"Отрезок после увеличения на {incrementValue}: {incrementedSegment}");
+        Console.WriteLine("");
+
+        // Проверка, попадает ли число в новый отрезок
+        if (incrementedSegment < (int)number)
+        {
+            Console.WriteLine($"Число {number} попадает в увеличенный отрезок.");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} не попадает в увеличенный отрезок.");
+        }
+    }
+}
+
